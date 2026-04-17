9c0e3306a147fae6c7dfb594ccff1d9dcaf0d682
Vinicius-de-Castro <viniciuscastro0711@gmail.com>
2026-04-17 10:54:59 -0300

    Initial Commit

---
+++ koala/koalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  koalaApp.swift
+//  koala
+//
+//  Created by User on 17/04/26.
+//
+
+import SwiftUI
+
+@main
+struct koalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
